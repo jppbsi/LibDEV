1cfb9f034046eaa46adcd5511e3cea74dc87c68c
Joao Papa <papa@Joaos-MacBook-Air-2.local>
2016-06-23 20:32:08 -0300

    Fixed bug at examples/OPF/OPFknn.c

--- examples/OPF/OPFknn.c
+++ examples/OPF/OPFknn.c
@@ -7,13 +7,17 @@ int main(int argc, char **argv){
     }
     
     SearchSpace *s = NULL;
+    int kmax = atoi(argv[3]);
     
     s = CreateSearchSpace(100, 1, _PSO_);
     s->w = 1.4;
     s->c1 = 1.4;
     s->c2 = 0.6;
     s->iterations = 10;
+    s->LB[0] = 1;
+    s->UB[0] = kmax;
     
+    InitializeSearchSpace(s, _PSO_); /* It initalizes the search space */
     
     DestroySearchSpace(&s);
     
